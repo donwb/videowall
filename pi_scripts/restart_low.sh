f92a4927bdae13fe013c2278e1d5fe0f573ad61c
Don Browning <donwb@2021-14-ProMax.local>
2022-01-01 23:42:20 -0500

    scripts for the pi

---
+++ pi_scripts/restart_low.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "killing Chromium"
+ps -ef | grep chromium-browser | grep -v grep | awk '{print $2}' | xargs kill
+
+echo "restarting....."
+DISPLAY=:0 "/usr/bin/chromium-browser" --start-fullscreen --start-maximized https://videowall-kb9x9.ondigitalocean.app/hdlow &
